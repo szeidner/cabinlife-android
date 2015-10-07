2447343de291d3fdc22deaf3df0ededc0f5c5fda
Steve Zeidner <steve.zeidner@dynamit.com>
2015-10-07 12:57:29 -0400

    Add library dependencies.

--- app/src/main/java/com/stevezeidner/cabinlife/MainActivity.java
+++ app/src/main/java/com/stevezeidner/cabinlife/MainActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.crashlytics.android.Crashlytics;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Crashlytics.start(this);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
